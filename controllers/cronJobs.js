1b0ce8a406c2dba6756464b28519f68d6e986347
Adonis Willer <nmtung.study@gmail.com>
2024-04-21 05:25:50 +0700

    Add telegram webhook 3

--- controllers/cronJobs.js
+++ controllers/cronJobs.js
@@ -80,7 +80,7 @@ const getEventPending = async () => {
     if (dataItem !== undefined && dataItem !== null && dataItem.length > 0) {
         fetch(`https://api.telegram.org/bot${dataItem[0].TOKEN}/sendMessage`, {
             method: 'POST',
-            body: JSON.stringify(dataItem[0].C_MESSAGE),
+            body: JSON.parse(dataItem[0].C_MESSAGE),
         })
           .then(res => res.json())
           .then(data => {
