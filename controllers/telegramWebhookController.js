cbca424a9fd607449e110b8bdfbcca9062bcbc9e
Adonis Willer <nmtung.study@gmail.com>
2024-04-20 22:31:42 +0700

    Add telegram webhook

--- controllers/telegramWebhookController.js
+++ controllers/telegramWebhookController.js
@@ -5,47 +5,34 @@ class GatewayController {
 		const dataUser = req.dataUser;
 		const data = req.body;
 
-		console.log(data)
+		let connection
+		try {
+			oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+			connection = await oracledb.getConnection('admin');
 
-		// let connection
-		// try {
-		// 	oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
-		// 	connection = await oracledb.getConnection('admin');
-		//
-		// 	let resultDb = await connection.execute(
-		// 		`
-    //     BEGIN
-    //         pkg_api.main_api_webhook(:user, :cmd, :data, :result);
-    //     END;`,
-		// 		{
-		// 			user: { dir: oracledb.BIND_IN, type: oracledb.STRING, val: dataUser.username},
-		// 			cmd: { dir: oracledb.BIND_IN, type: oracledb.STRING, val: cmd},
-		// 			data: { dir: oracledb.BIND_IN, type: oracledb.CLOB, val: JSON.stringify(data)},
-		// 			result: { dir: oracledb.BIND_OUT, type: oracledb.CURSOR, maxSize: 5267}
-		// 		}
-		// 	);
-		//
-		// 	let dataRes = await convertResultDbToArray(resultDb);
-		//
-		// 	if (dataRes.length === 1 && dataRes[0].MESSAGE_ERROR != null) {
-		// 		return res.status(400).json({
-		// 			error_message: dataRes[0].MESSAGE_ERROR.replace(/ORA-\d{5}: /g, ''),
-		// 		});
-		// 	}
-		//
-		// 	return res.json({data: dataRes});
-		// } catch (error) {
-		// 	return res.json({error_message: error.message});
-		// } finally {
-		// 	if (connection) {
-		// 		try {
-		// 			await connection.close();
-		// 		} catch (err) {
-		// 			console.error(err.message);
-		// 		}
-		// 	}
-		// }
-		return res.json({data: ''});
+			let resultDb = await connection.execute(
+				`
+        BEGIN
+            pkg_api.main_api_webhook(:data);
+        END;`,
+				{
+					data: { dir: oracledb.BIND_IN, type: oracledb.CLOB, val: JSON.stringify(data)}
+				}
+			);
+
+			return res.json({data: ''});
+		} catch (error) {
+			console.log(error)
+			return res.json({data: ''});
+		} finally {
+			if (connection) {
+				try {
+					await connection.close();
+				} catch (err) {
+					console.error(err.message);
+				}
+			}
+		}
 	}
 }
 
