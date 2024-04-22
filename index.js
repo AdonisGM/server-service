daf8bf36845a250fbf01ec256cf2cdb4cc951f1d
Nguyễn Mạnh Tùng <63092029+FPT-NMTung@users.noreply.github.com>
2024-04-22 12:26:48 +0700

    Update

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var corsOptions = {
 };
 app.use(json({limit: '2mb'}))
 app.use(cors(corsOptions))
-app.use(logger('combined'));
+app.use(logger('short'));
 app.use(cookieParser())
 
 app.use('/account', authRouter);
