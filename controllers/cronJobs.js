8ade8f09a6afccfcf9a6b4f8b58e5d55103e7853
AdonisGM <nmtung.study@gmail.com>
2024-05-09 09:50:11 +0700

    Update event

--- controllers/cronJobs.js
+++ controllers/cronJobs.js
@@ -1,13 +1,13 @@
 const schedule = require('node-schedule');
 const oracledb = require('oracledb');
-const connectDatabase = require("../modal/database");
+const connectDatabase = require('../modal/database');
 
 const abc = schedule.scheduleJob('*/1 * * * * *', function () {
-    getEventPending(); 
+    getEventPending();
 });
 
 const getEventPending = async () => {
-    const dataItem = await connectDatabase('pkg_tele_management.get_event', {})
+    const dataItem = await connectDatabase('pkg_tele_management.get_event', {});
 
     if (dataItem !== undefined && dataItem !== null && dataItem.length > 0) {
         if (dataItem[0].MESSAGE_ERROR) {
@@ -20,28 +20,35 @@ const getEventPending = async () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-        }
+        };
 
-        fetch(`https://api.telegram.org/bot${dataItem[0].TOKEN}${dataItem[0].C_METHOD}`, data)
-          .then(res => res.json())
-          .then(res => {
-              console.log('---- Send notification ----', data);
-              console.log(res);
-              // try {
-              //     const dataItem1 = await connectDatabase('pkg_tele_management.update_event', {
-              //         pk_event: dataItem[0].PK_TELE_SEND_MESSAGE,
-              //         res: JSON.stringify(res)
-              //     })
-              // } catch (error) {
-              //     console.log(error)
-              // }
-          })
-          .catch(err => {
-              console.log(err)
-              // const dataItem2 = await connectDatabase('pkg_tele_management.update_event', {
-              //     pk_event: dataItem[0].PK_TELE_SEND_MESSAGE,
-              //     res: JSON.stringify(err)
-              // })
-          });
+        fetch(
+            `https://api.telegram.org/bot${dataItem[0].TOKEN}${dataItem[0].C_METHOD}`,
+            data
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                try {
+                    const dataItem1 = connectDatabase('pkg_tele_management.update_event', {
+                        pk_event: dataItem[0].PK_TELE_SEND_MESSAGE,
+                        res: JSON.stringify({
+							data: data,
+							res: res
+						})
+                    })
+                } catch (error) {
+                    console.log(error)
+                }
+            })
+            .catch((err) => {
+                try {
+					const dataItem2 = connectDatabase('pkg_tele_management.update_event', {
+						pk_event: dataItem[0].PK_TELE_SEND_MESSAGE,
+						res: err.message
+					})
+				} catch (error) {
+					console.log(error)
+				}
+            });
     }
-}
+};
